3ecdc0162c7888c810a085c4486f3b5da9f27c57
Joyion Timmons <joyiont@gmail.com>
2024-09-25 18:25:26 -0500

    Fixes active link to change on scrolling

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const navLinks = document.querySelector(".nav-links")
+const navLinks = document.querySelector(".nav-links .link")
 const burgerIcon = document.querySelector(".burger-icon");
+const sections = document.querySelectorAll('.main-section');
 
+const aLinks = document.querySelectorAll(".nav-links a");
 function openNavBar() {
 
     burgerIcon.addEventListener("click", () => {
@@ -10,5 +12,51 @@ function openNavBar() {
     })
 
 }
+
+let observerOptions = {
+    root: null,
+    rootMargin: '-50% 0px -50% 0px',
+    threshold: 0,
+}
+
+
+const observerCallback = ((entries) => {
+    entries.forEach((entry) => {
+        // console.log(entry.target);
+        let elem = entry.target;
+        if (entry.isIntersecting) {
+
+            const linkId = entry.target.id;
+            console.log(elem.getAttribute("id"));
+
+            aLinks.forEach((l) => {
+                if ((l.getAttribute("href")) == "#" + linkId) {
+                    l.classList.add("active-link");
+
+
+                } else {
+                    l.classList.remove('active-link');
+
+                }
+
+
+            })
+        } {
+
+        }
+    })
+
+
+})
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
+
 
 openNavBar();
