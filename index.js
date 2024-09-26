9b62626bc5893ca3aada879165b4ea129d9d44f0
Joyion Timmons <joyiont@gmail.com>
2024-09-25 19:48:10 -0500

    Tempeorary fix for mobile navigation

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const navLinks = document.querySelector(".nav-links .link")
 const burgerIcon = document.querySelector(".burger-icon");
 const sections = document.querySelectorAll('.main-section');
+const navLinkSection = document.querySelector(".nav-links");
 
 const aLinks = document.querySelectorAll(".nav-links a");
 function openNavBar() {
 
     burgerIcon.addEventListener("click", () => {
-        console.log("Clicking");
-        navLinks.classList.toggle("nav-active");
-
+        navLinkSection.classList.toggle("nav-closed")
     })
 
 }
@@ -27,7 +26,7 @@ const observerCallback = ((entries) => {
         if (entry.isIntersecting) {
 
             const linkId = entry.target.id;
-            console.log(elem.getAttribute("id"));
+
 
             aLinks.forEach((l) => {
                 if ((l.getAttribute("href")) == "#" + linkId) {
